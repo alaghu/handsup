1e2b227281f4943f9bf56ea328634f9420296e9f
Anand Alaghu <anand@mannerweb.com>
2015-03-10 04:51:59 +0000

    First commit.

---
+++ Labels.rb
@@ -0,0 +1,18 @@
+# This script will attempt to open a text file and parse its contents
+# The script should identify different types of 
+# 10 Lines - a. Remarks or comments , b. Label details , and c. Other
+
+# Depending on the line type, do the following
+# 10a. Line Type = Comments
+#      ignore. Do not parse
+
+# 10b. Line Type = Label details. 
+#       Parse and get the following 
+#       Product Line
+#       Label 
+#       Other Details
+
+# 10c. Line Type = Others
+#      marke as other and store. Do not parse.
+
+
