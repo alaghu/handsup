0bd352a4b91350d16508aa85a8fcf760c168a2c6
Anand Alaghu <anand@mannerweb.com>
2015-03-13 05:46:32 +0000

    rubocop clean

--- lib/label.rb
+++ lib/label.rb
@@ -1,15 +1,15 @@
 # This script will attempt to open a text file and parse its contents
-# The script should identify different types of 
+# The script should identify different types of
 # 10 Lines - a. Remarks or comments , b. Label details , and c. Other
 
 # Depending on the line type, do the following
 # 10a. Line Type = Comments
 #      ignore. Do not parse
 
-# 10b. Line Type = Label details. 
-#       Parse and get the following 
+# 10b. Line Type = Label details.
+#       Parse and get the following
 #       Product_Schema
-#       Label 
+#       Label
 #       Other Details
 #       Label_Humanized
 #       Date_Parsed
@@ -17,7 +17,6 @@
 
 # 10c. Line Type = Others
 #      marke as other and store. Do not parse.
-
 
 # Model it as the following
 # A. The whole file/Label = Label
@@ -29,24 +28,19 @@
 # Attributes on Label
 # Date_Parsed
 # LabelName (The first Line)
-# 
+#
 
 #
 class Label
-    
-    #to retrieve the label from the first line
-    def retrieve_label_from_first_line(string)
-        
-        #obtaining string prior to fullsource
-        pre_fullsource = string.match('fullsource').pre_match
-      
-        #obtaining string post '# ', which is the beginning of the line
-        post_initial_chars = pre_fullsource.match('# ').post_match
-     
-        #removing any unwanted spaces
-        label = post_initial_chars.strip
-        
-        return label
-    end
-    
+  # to retrieve the label from the first line
+  def retrieve_label_from_first_line(string)
+    # obtaining string prior to fullsource
+    pre_fullsource = string.match('fullsource').pre_match
+
+    # obtaining string post '# ', which is the beginning of the line
+    post_initial_chars = pre_fullsource.match('# ').post_match
+
+    # removing any unwanted spaces
+    post_initial_chars.strip
+  end
 end
