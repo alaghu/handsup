67f81a5f371c34ad0e9cb2170be3f2bb4e9d89eb
Anand Alaghu <anand@mannerweb.com>
2015-03-18 05:29:36 +0000

    initialize and using attribute reader.

--- lib/label.rb
+++ lib/label.rb
@@ -1,48 +1,31 @@
+# A gem that makes Regular Expression as english statements
 require 'verbal_expressions'
-# This script will attempt to open a text file and parse its contents
-# The script should identify different types of
-# 10 Lines - a. Remarks or comments , b. Label details , and c. Other
 
-# Depending on the line type, do the following
-# 10a. Line Type = Comments
-#      ignore. Do not parse
-
-# 10b. Line Type = Label details.
-#       Parse and get the following
-#       Product_Schema
-#       Label
-#       Other Details
-#       Label_Humanized
-#       Date_Parsed
-#       Product_Type
-
-# 10c. Line Type = Others
-#      marke as other and store. Do not parse.
-
-# Model it as the following
-# A. The whole file/Label = Label
-# B. Each line = Lines
-
-# Validations on Label
-# If First line of the label does not contain a formed label, return error.
-#
-# Attributes on Label
-# Date_Parsed
-# LabelName (The first Line)
-#
-
-#
+# The Label class
 class Label
   # This class will attempt to decipher the label
   # Should have the following methods
+
   # retrieve_segments_from_label
   # humanize_label_date
   # validate_label
-  # 
+
+  DEFAULT_VALUE = 'Blank'
+
+  attr_reader :series, :version, :platform, :date
+
+  def initialize
+    @series = DEFAULT_VALUE
+    @version = DEFAULT_VALUE
+    @platform = DEFAULT_VALUE
+    @date = DEFAULT_VALUE
+  end
+
   # To retrieve the label from the first line
   # The first_line will be some thing like
   # 'FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550 fullsource file'
   # Would return => 'FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550'
+  # TODO: Move this method to a differen class
   def retrieve_label_from_first_line(first_line)
     # obtaining first_line prior to fullsource
     pre_fullsource = first_line.match('fullsource').pre_match
@@ -96,3 +79,34 @@ class Label
     end
   end
 end
+
+# This script will attempt to open a text file and parse its contents
+# The script should identify different types of
+# 10 Lines - a. Remarks or comments , b. Label details , and c. Other
+
+# Depending on the line type, do the following
+# 10a. Line Type = Comments
+#      ignore. Do not parse
+
+# 10b. Line Type = Label details.
+#       Parse and get the following
+#       Product_Schema
+#       Label
+#       Other Details
+#       Label_Humanized
+#       Date_Parsed
+#       Product_Type
+
+# 10c. Line Type = Others
+#      marke as other and store. Do not parse.
+
+# Model it as the following
+# A. The whole file/Label = Label
+# B. Each line = Lines
+
+# Validations on Label
+# If First line of the label does not contain a formed label, return error.
+#
+# Attributes on Label
+# Date_Parsed
+# LabelName (The first Line)
--- spec/label_spec.rb
+++ spec/label_spec.rb
@@ -2,19 +2,35 @@ require 'label'
 
 # TODO: - need to refactor - single object creation
 describe Label, '#label' do
+  # TODO: This method should be moved to a different class altogether
   it 'should come back with a label' do
     label = Label.new
     # TODO: variabilize the input text, preferebally from a file
+
     output = label.retrieve_label_from_first_line('# FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550 fullsource file, generated Sat Jan  3 16:03:00 2015 by aime1')
+
     expect(output).to eq('FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550')
   end
+
+  # This is for testing initialization
+  it 'The  Series,version, platform, and date should set to Blank' do
+    a_new_label = Label.new
+
+    expect(a_new_label.series).to eq('Blank')
+    expect(a_new_label.version).to eq('Blank')
+    expect(a_new_label.platform).to eq('Blank')
+    expect(a_new_label.date).to eq('Blank')
+  end
+
   it 'should come back with 03-Jan-2015' do
     label = Label.new
     date_from_label = label.retrieve_date_from_label('FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550')
     expect(date_from_label).to eq('150103.1550')
   end
+
   it 'should check for 3 underscores as a prereq for a lable' do
     label = Label.new
+
     number_of_underscore = label.validate_label_pattern('FAINTEG_11.1.9.2.0_PLATFORMS_150103.1550')
   end
 end
